b7ccd0dfa898cc3f94f0bb7f7c7403576545ce0b
Jani Turunen <turunen.jani@gmail.com>
2014-11-12 23:41:26 +0200

    tweak

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,6 @@ require 'pry'
 
 Capybara.configure do |config|
   config.default_driver = :selenium
-  config.app_host   = 'http://fuzu:fuzudemo2014@www.fuzu.com'
+  config.app_host   = 'http://www.fuzu.com'
 end
 World(Capybara)
